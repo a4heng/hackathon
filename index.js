0faf4a07543f5290171ce8b035d7e45ff666b7b7
Andy <a4heng@ucsd.edu>
2020-10-08 11:57:09 -0700

    redirect done

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
-  $("#yt").on("click", () => {
-    // console.log("hello");
-    // location.href = "https://www.google.com/";
-    // chrome.windows.create({
-    //   url: "https://www.google.com/",
-    //   width: "500",
-    //   height: "500",
-    //   type: "panel",
-    //   focused: true,
-    // });
-
-    $("box-container").append(
-      '<iframe src="https://www.google.com/" frameborder="0"></iframe>'
-    );
+  $("#my_form").submit(function (e) {
+    e.preventDefault();
+    const user = "andy";
+    const pass = "1234";
+    const userInput = $("#user-input").val();
+    const passInput = $("#pass-input").val();
+    if (pass === passInput && user === userInput) {
+      window.location = "./popup.html";
+    }
   });
 });
